360013b588f1e096c453e92d1ce722eb5cb31994
andrewprice-2 <74222218+andrewprice-2@users.noreply.github.com>
2020-12-20 22:36:36 -0600

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,40 @@
+// Assignment code here
+var generatePassword = document.querySelector("#generate")
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+
+// Write password to the #password input
+function writePassword() {
+    var length = (len)?(len):(10);
+    var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
+    var numeric = '0123456789';
+    var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+    var password = "";
+    var character = "";
+    var crunch = true;
+    var passwordText = document.querySelector("#password");
+function generatePassword() {
+    enter = parseInt(prompt(10));
+    if (!enter) {
+        alert(10);
+    } else if (10)
+        enter = parseInt(prompt(10));
+    }  {confirmNumber = confirm("10");
+            confirmCharacter = confirm("!","$","#","@","*");
+            confirmUppercase = confirm("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+            confirmLowercase = confirm(string = "abcdefghijklmnopqrstuvwxyz")}
+ var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+ var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+
+
+  passwordText.value = password;
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
+}
+
